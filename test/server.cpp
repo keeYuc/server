a047befa9efcad7d2cef7d16ee09386f5431aada
keeYuc <52500690@qq.com>
2020-11-11 20:50:19 +0800

    master

---
+++ test/server.cpp
@@ -0,0 +1,19 @@
+#include"../src/mes.hpp"
+
+
+
+int main()
+{
+    mySocket s;
+    int fd = accept(s.myListen(), nullptr, nullptr);
+    while (1)
+    {
+        
+    }
+
+
+
+
+
+    return 0;
+}
