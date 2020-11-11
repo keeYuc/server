46a680076ea6c127e6db9365d9dc7057a638f573
keeYuc <52500690@qq.com>
2020-11-11 18:10:52 +0800

    master

--- trans_File/client.cpp
+++ trans_File/client.cpp
@@ -20,26 +20,44 @@ int main()
     addr.sin_port = htons(5555);
     inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
     connect(fd, (sockaddr*) &addr, sizeof(addr));//
-    std::fstream file("./timg.jpg", std::ios::in | std::ios::binary);
+    std::fstream file("./fucker.png", std::ios::in | std::ios::binary);
     file.seekg(0, file.end);
-    int n = file.tellp();
+    int n = file.tellg();
     // char buff[n];
     // 
     file.seekg(0, std::ios::beg);
-    char buff[n];
-
-    file.read(buff, n);
-    std::cout << n;
-
-    char a[8];
+    char a[12];
     // char b[n];
-    bzero(a, 8);
+    bzero(a, 12);
     // bzero(b, n);
     sprintf(a, "%d", n);
-    write(fd, a, 8);
-    write(fd, buff, n);
+    write(fd, a, 12);std::cout << n;
+    int sum;
+    int trans_max = 1024 * 256;
+    for (;file.tellg() < n;)
+    {
 
+        if (trans_max < (n - file.tellg()))
+        {
+            char buff[trans_max];
+            bzero(buff, trans_max);
+            file.read(buff, trans_max);
+            write(fd, buff, trans_max);
+            sum += trans_max;
+            printf("----send[%d]-----sum:%d\n", trans_max, sum);
 
+        } else
+        {
+            int last = n - file.tellg();
+            sum += last;
+            char buff[last];
+            bzero(buff, last);
+            file.read(buff, last);
+            write(fd, buff, last);
+            printf("----send[%d]-----sum:%d\n", last, sum);
+
+        }
+    }
 
     return 0;
 }
--- trans_File/main.cpp
+++ trans_File/main.cpp
@@ -1,4 +1,4 @@
-#include"./src/mes.hpp"
+#include"../src/mes.hpp"
 #include<pthread.h>
 #include<string.h>
 #include<stdio.h>
@@ -20,6 +20,7 @@ int main()
     epoll_ctl(tree, EPOLL_CTL_ADD, fd, &event);
     while (1)
     {
+    abc:
         int n = epoll_wait(tree, events, 1025, -1);
         for (int i = 0;i < n;i++)
         {
@@ -34,38 +35,50 @@ int main()
                 event1.data.fd = afd;
                 epoll_ctl(tree, EPOLL_CTL_ADD, afd, &event1);
                 //*accept thread
-
-
-
             } else
             {
                 //*test
                 char buff[1024];
 
-                while (1)
+                system("rm ./fuck.png");
+                //sleep(2);
+                std::fstream file("./fuck.png", std::ios::app | std::ios::binary);
+                bzero(buff, 12);
+                n = read(x, buff, 12);
+                int q = atoi(buff);
+                printf("%d\n", q);
+                int sum;
+                int max = q;
+                while (n < q)
                 {
+                    if (q > 1024 * 256)
+                    {
+                        max = 1024 * 256;
+                    }
 
-                    bzero(buff, 8);
-                    n = read(x, buff, 8);
-                    int q = atoi(buff);
-                    printf("%d\n", q);
-                    char temp[q];
-                    bzero(temp, q);
-                    n = read(x, temp, q);
-                    printf("[%d]\n", n);
+                    char temp[max];
+                    bzero(temp, max);
+                    n = read(x, temp, max);
+                    file.write(temp, n);
+                    sum += n;
+                    printf("[%d]---sum[%d]\n", n, sum);
                     if (n <= 0)
                     {
-                        printf("lost link\n");
+                        printf("lost link------lost:%d\n", q - sum);
                         epoll_ctl(tree, EPOLL_CTL_DEL, x, &events[i]);
-                        break;
+                        file.close();
+                        return 0;
 
                     }
-                    std::fstream file("./fuck.jpg", std::ios::app | std::ios::binary);
-                    file.write(temp, q);
-                    file.close();
-                    printf("%s\n", temp);
+
                 }
-                //*trans thread
+                epoll_ctl(tree, EPOLL_CTL_DEL, x, &events[i]);
+                file.close();
+                return 0;
+
+                //printf("%s\n", temp);
+
+            //*trans thread
 
             }
         }
