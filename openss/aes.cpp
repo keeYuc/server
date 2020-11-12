a7b7804fac8b649f7523f15dd935f3826676e726
keeYuc <52500690@qq.com>
2020-11-12 14:17:17 +0800

    master

---
+++ openss/aes.cpp
@@ -0,0 +1,11 @@
+#include<openssl/aes.h>
+# define AES_BLOCK_SIZE 32
+
+
+int main(){
+    AES_KEY abc;
+    abc.
+    
+    
+    return 0;
+}
--- openss/main.cpp
+++ openss/main.cpp
@@ -10,12 +10,16 @@ int main()
     auto num = BN_new();
     BN_set_word(num, 12345);
     //printf("s");
+    char buff[11] = "accdefg123";
+    char buff1[11];
+    char buff2[11];
+    RSA_generate_key_ex(rsa, 1024, num, nullptr);
     auto pub = RSAPublicKey_dup(rsa);
     auto pri = RSAPrivateKey_dup(rsa);
-
-    RSA_generate_key_ex(rsa, 1024, num, nullptr);
-    
-
+    //printf("%s\n", (char*) pub);printf("%s\n", (char*) pri);
+    RSA_public_encrypt(11, (unsigned char*) buff, (unsigned char*) buff1, pub, RSA_PKCS1_PADDING);
+    RSA_private_decrypt(11, (unsigned char*) buff1, (unsigned char*) buff2, pri, RSA_PKCS1_PADDING);
+    printf("%s\n", buff2);
     RSA_free(rsa);
 
     return 0;
