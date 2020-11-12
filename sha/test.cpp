cf5429029787c243a69b21f8fd5d17ebdeb6ab7a
keeYuc <52500690@qq.com>
2020-11-12 15:09:54 +0800

    master

--- sha/test.cpp
+++ sha/test.cpp
@@ -1,14 +1,17 @@
 #include<string.h>
 #include<openssl/sha.h>
 #include<iostream>
-
+#include"../base64/base64.hpp"
 
 
 int main()
 {
     char buff[64];
     char a[4] = "abc";
-    SHA256((unsigned char*)a, 4, (unsigned char*) buff);
+    SHA256((unsigned char*) a, 4, (unsigned char*) buff);
+    Base64Encrypt a;
+    
+
     printf("%s\n", buff);
     return 0;
 }
