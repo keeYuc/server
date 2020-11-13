d299ceb70c6a5612c9db2682311a2f362d67ac51
keeYuc <52500690@qq.com>
2020-11-13 21:03:53 +0800

    master

--- myBase64/test.cpp
+++ myBase64/test.cpp
@@ -7,9 +7,11 @@ int main()
 {
 
     std::string abc = "      asdas                 dadad          ";
+    printf("[%s][%ld]\n", abc.data(), abc.size());
     std::cout << abc.size() << std::endl;
-    auto str=base64Encode(abc.data(), abc.size(), false);
-    printf("[%s][%ld]\n",str,strlen(str)+1);
+    auto str = base64Encode(abc.data(), abc.size(), false);
+    auto str1 = base64Decode(str, strlen(str) + 1, false);//!decode需要strlen+1
+    printf("[%s][%ld]\n", str1, strlen(str1));          //*之后【不需要】strlen+1
 
     return 0;
 }
