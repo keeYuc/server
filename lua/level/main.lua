48c0894306e36664a49a7d2e71e4d5e7a5220742
root <root@DESKTOP-QNN3LR1.localdomain>
2020-11-25 20:16:31 +0800

    master

--- lua/level/main.lua
+++ lua/level/main.lua
@@ -2,4 +2,18 @@
 -- local str=io.popen("pwd")
 -- str=str:read("a")
 -- package.path=package.path..";"..str..";"
-local tool =require"tool"
+-- local tool =require"tool"
+
+local test={name="abc"}
+function test:test(str)
+    if not str then
+        str=self.name
+    end
+    print(str)
+end
+function aaaa()
+    print("aaaa")
+end
+test:test()
+
+
